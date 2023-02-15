1bc6bc4529a859deba739e451ccd08db2718e0aa
Denis-Shupletsov <b128k2351763@gmail.com>
2023-02-15 21:03:11 +0200

    js

--- modul4/autoпроверка.js
+++ modul4/autoпроверка.js
@@ -16,6 +16,65 @@
 // const result = makePizza();
 // const pointer = makePizza;
 
+
+
 2
+// Дополни функцию makeMessage так, чтобы она ожидала вторым параметром(параметр callback) 
+// колбэк - функцию и возвращала ее вызов.
+// Функция deliverPizza или makePizza будет передаваться как колбэк и 
+// ожидать аргументом имя готовой доставляемой пиццы.
+
+// function deliverPizza(pizzaName) {
+//     return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//     return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+
+//     return callback(pizzaName);
+// }
+// makeMessage("Ultracheese", deliverPizza);
+// makeMessage("Royal Grand", makePizza);
+
+3
+// Дополни второй вызов функции makePizza(pizzaName, callback),
+// передав вторым аргументом инлайн колбэк - функцию eatPizza(pizzaName),
+// которая логирует строку "Eating pizza <имя пиццы>"..
+
+// function makePizza(pizzaName, callback) {
+//     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//     callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//     console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//     console.log(`Eating pizza ${pizzaName}.`);
+// });
+
+// Пример из конспекта
+
+// function registerGuest(name, callback) {
+//     console.log(`Регистрируем гостя ${name}.`);
+//     callback(name);
+// }
+
+// // Передаём инлайн функцию greet как колбэк
+// registerGuest("Манго", function greet(name) {
+//     console.log(`Добро пожаловать ${name}.`);
+// });
+
+// // Передаём инлайн функцию notify как колбэк
+// registerGuest("Поли", function notify(name) {
+//     console.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
+// });
 
 
+4
